3784a6c9103edb97ab00e5d1bc3bb9426338c81c
Lyjia <tom.corelis@gmail.com>
2025-08-17 22:09:34 -0700

    remove problematic import and assert for now

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -12,12 +12,12 @@ def test_imports():
     from models.settings import settings
     from models.qt.metadata_model import MetadataTableModel
     from providers.metadata.mutagen_provider import MutagenProvider
-    from windows.main_window import MainWindow
+    # from windows.main_window import MainWindow # removed as this crashes github runner with:  ImportError: libEGL.so.1: cannot open shared object file: No such file or directory
     from workers.gui.metadata_loader import MetadataLoader
 
     assert MediaFile is not None
     assert settings is not None
     assert MetadataTableModel is not None
     assert MutagenProvider is not None
-    assert MainWindow is not None
+    # assert MainWindow is not None
     assert MetadataLoader is not None
