078523e8322012cdae920805ee0394a1c98fa219
Lyjia <tom.corelis@gmail.com>
2025-08-20 16:27:38 -0700

    fix broken logger

--- src/util/logging.py
+++ src/util/logging.py
@@ -2,14 +2,15 @@ import logging
 import sys
 
 
-def setup_logging(log_name = 'YAAMT', log_level = logging.DEBUG):
-    hand = logging.StreamHandler( sys.stderr )
-    hand.setLevel(log_level)
-    hand.setFormatter(logging.Formatter('%(asctime)s-%(levelname)s-%(name)s: %(message)s'))
+def setup_logging(log_name='YAAMT', log_level=logging.DEBUG):
+    hand = logging.StreamHandler(sys.stderr)
+    hand.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s'))
 
     logger = logging.getLogger(name=log_name)
+    logger.setLevel(log_level)
     logger.addHandler(hand)
 
     return logger
+
 
 log = setup_logging()
