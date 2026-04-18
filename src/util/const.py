c8e0d9a358a32d11cc918e5222b9e8ba56c5b34f
Lyjia <tom.corelis@gmail.com>
2026-04-17 21:56:21 -0700

    update example rename formatting presets

--- src/util/const.py
+++ src/util/const.py
@@ -314,9 +314,10 @@ RENAME_COLLISION_MODE_LABELS = {
 RENAME_PRESETS_DEFAULTS = [
     "%ARTIST% - %TITLE%",
     "%ARTIST% - %TITLE%< (%REMIXER% Remix)>",
-    "<%TRACKNUMBER:00% - >%ARTIST% - %TITLE%",
-    "<<%ALBUMARTIST% - >%ALBUM% - %TRACKNUMBER:00%> %ARTIST% - %TITLE%",
+    "<%TRACKNUMBER:00%. >%ARTIST% - %TITLE%",
+    "<[<%ALBUMARTIST% - ><%ALBUM%>< - %TRACKNUMBER:00%>]> %ARTIST% - %TITLE%< (%REMIXER% Remix)>",
     "<%YEAR% - >%ARTIST% - %ALBUM% - %TRACKNUMBER:00% - %TITLE%",
+    "<%INITIALKEY%,><%BPM:000%,>%ARTIST% - <%ALBUM% - ><%TRACKNUMBER:00% - >%TITLE%< (%REMIXER% Remix)>"
 ]
 
 # Hand-curated sample data used by the rename setup dialog's live "Example:" label.
